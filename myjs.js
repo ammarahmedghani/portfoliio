f3c1d90d3bc22a772fca72511d1135bd4e41eba9
ammarahmedghani <ammarghani23@gmail.com>
2020-07-07 15:21:46 +0500

    navedit

--- myjs.js
+++ myjs.js
@@ -150,3 +150,15 @@ console.log(innerh)
 $('.effect').hide();
 
 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector('.my-nav');
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
